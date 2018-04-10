ad6d3fece8c6e436c659ff51b6f75d1349795814
Rafael Bernardo <rafamaxber@gmail.com>
2018-04-09 22:31:36 -0300

    change index component to functional component

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,44 +1,24 @@
-import React from 'react'
-import { withPrefix } from 'gatsby-link'
-import styled from 'styled-components'
-import {Container} from '../Shared'
+import React from 'react';
+import styled from 'styled-components';
+import { Container } from '../Shared';
+
+import gtMax3d from '../../img/GTMax3D.jpg';
 
 const Banner = styled.section`
   width: 100%;
-  min-height: 500px;
-  background-image: url(${withPrefix("images/GTMax3D.jpg")});
+  height: 500px;
   background-repeat: no-repeat;
-  background-size: cover;
+  background-image: url(${gtMax3d});
   position: relative;
-  background-position: center -12%;
   background-attachment: fixed;
   background-color: #070707;
-  ::after {
-    content: "";
-    background-color: rgba(0, 0, 0, 0.70);
-    position: relative;
-    top: 0;
-    left: 0;
-    display: block;
-    width: 100%;
-    height: 100%;
-  }
+  background-position: 0 -20%;
+  background-size: 100%;
 `;
 
 const BannerContainer = styled(Container)`
   position: relative;
-`
-
-const BannerTextTitle = styled.div`
-  font-size: 6.0em;
-  font-weight: bold;
-`
-
-const BannerText = styled.div`
-  font-size: 3.0em;
-  width: 60%;
-  margin-top: 2.0px;
-`
+`;
 
 const BannerContainerText = styled.div`
   position: absolute;
@@ -49,54 +29,14 @@ const BannerContainerText = styled.div`
   bottom: 0;
   width: 100%;
   height: 300px;
-`
+`;
 
-const Arrow = styled.a`
-  display: block;
-  position: absolute;
-  bottom: 30px;
-  z-index: 1;
-  width: 40px;
-  background-color: #fff;
-  margin: auto;
-  left: 0;
-  right: 0;
-  border-radius: 50%;
-  padding: 7px;
-  cursor: pointer;
-`
-
-const Image = styled.img`
-  width: 100%;
-  position: relative;
-  animation: slideDown 0.7s infinite;
-  @keyframes slideDown {
-    from {
-      top: 0;
-      opacity: 1;
-    }
-    to {
-      top: 10px;
-      opacity: 0;
-    }
-  }
-`
-
-const Component = ({ goTo }) => (
-  <Banner data-js="banner-hero">
+const Component = () => (
+  <Banner>
     <BannerContainerText>
-      <BannerContainer>
-        <BannerTextTitle>BEM-VINDO A AW3D</BannerTextTitle>
-        <BannerText>
-          É com muito prazer que a AW3D lhe apresenta o mundo da Impressão 3D e
-          pode acreditar, você não vai conseguir viver mais sem ele.
-        </BannerText>
-      </BannerContainer>
+      <BannerContainer />
     </BannerContainerText>
-    <Arrow onClick={goTo}>
-      <Image src={withPrefix("images/icons8-down-arrow-filled-50.png")} />
-    </Arrow>
   </Banner>
 );
 
-export default Component
+export default Component;
