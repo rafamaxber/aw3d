4e3dec30b05e8e2c59dd038a09f8dc6b96bc38c3
Rafael Bernardo <rafamaxber@gmail.com>
2018-04-17 13:42:30 -0300

    wip

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,9 +4,11 @@ import SobrePagePreview from './preview-templates/SobrePagePreview';
 import BlogPostPreview from './preview-templates/BlogPostPreview';
 import ProductPagePreview from './preview-templates/ProductPagePreview';
 import IndexPagePreview from './preview-templates/IndexPagePreview';
+import PortfolioPagePreview from './preview-templates/PortfolioPagePreview';
 
 CMS.registerPreviewStyle('/styles.css');
 CMS.registerPreviewTemplate('sobre', SobrePagePreview);
 CMS.registerPreviewTemplate('products', ProductPagePreview);
 CMS.registerPreviewTemplate('blog', BlogPostPreview);
+CMS.registerPreviewTemplate('portfolio', PortfolioPagePreview);
 CMS.registerPreviewTemplate('index', IndexPagePreview);
