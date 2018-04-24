076e9c64c495165272bb038eb4fd887f13df42fa
Rafael Bernardo <rafamaxber@gmail.com>
2018-04-24 18:44:41 -0300

    remove product page

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,12 +3,10 @@ import CMS from 'netlify-cms'; // eslint-disable-line import/no-extraneous-depen
 import SobrePagePreview from './preview-templates/SobrePagePreview';
 import ClientesPagePreview from './preview-templates/ClientesPagePreview';
 import BlogPostPreview from './preview-templates/BlogPostPreview';
-import ProductPagePreview from './preview-templates/ProductPagePreview';
 import IndexPagePreview from './preview-templates/IndexPagePreview';
 
 CMS.registerPreviewStyle('/styles.css');
 CMS.registerPreviewTemplate('sobre', SobrePagePreview);
 CMS.registerPreviewTemplate('clientes', ClientesPagePreview);
-CMS.registerPreviewTemplate('products', ProductPagePreview);
 CMS.registerPreviewTemplate('blog', BlogPostPreview);
 CMS.registerPreviewTemplate('index', IndexPagePreview);
