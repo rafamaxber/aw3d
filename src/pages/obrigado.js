02d9aa80303a6cee59d0f8c4b18ffadf4ec8942f
Rafael Bernardo <rafael.silva@digipix.com.br>
2018-06-08 19:23:30 -0300

    fix: google_conversion_id in /obrigado page

--- src/pages/obrigado.js
+++ src/pages/obrigado.js
@@ -3,10 +3,7 @@ import styled from 'styled-components';
 import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
 
-import {
-  Container,
-  Title,
-} from '../components/Shared';
+import { Container, Title } from '../components/Shared';
 
 import scriptConversionId from '../components/LeadDataLayer';
 
@@ -43,45 +40,42 @@ const Span = styled.span`
   display: inline-block;
   font-size: 1.5em;
 `;
-
+const javascriptCode = `
+  /* <![CDATA[ */
+  var google_conversion_id = 877644049;
+  var google_conversion_label = "ZZJHCMaA2nsQkZK_ogM";
+  var google_remarketing_only = false;
+  /* ]]> */
+`;
 const Obrigado = () => (
   <Wrapper>
     <Helmet title="Agradecemos o contato!" />
     <Title>
       <h1>Nós da AW3D agradecemos o contato!</h1>
     </Title>
-    <Text top>
-      Retornaremos o mais rápido possível com uma resposta.
-    </Text>
-    <Text>
-      Enquanto aguarda nossa resposta você pode:
-    </Text>
+    <Text top>Retornaremos o mais rápido possível com uma resposta.</Text>
+    <Text>Enquanto aguarda nossa resposta você pode:</Text>
     <ContainerBtns>
       <StoreBtn href="https://www.aw3dshop.com.br/?utm_source=site_institucional_obrigado">
         Conhecer nossa loja
       </StoreBtn>
-      <Span>
-        ou
-      </Span>
+      <Span>ou</Span>
       <Btn to="/" primary>
         Ir para a pagina inicial
       </Btn>
     </ContainerBtns>
-    <script type="text/javascript">
-      {`
-        /* <![CDATA[ */
-        var google_conversion_id = 877644049;
-        var google_conversion_label = "ZZJHCMaA2nsQkZK_ogM";
-        var google_remarketing_only = false;
-        /* ]]> */
-      `}
-    </script>
+    <script type="text/javascript" dangerouslySetInnerHTML={{ __html: javascriptCode }} />
 
-    <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js"></script>
+    <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js" />
 
     <noscript>
-      <div >
-        <img height="1" width="1" alt="" src="//www.googleadservices.com/pagead/conversion/877644049/?label=ZZJHCMaA2nsQkZK_ogM&amp;guid=ON&amp;script=0"/>
+      <div>
+        <img
+          height="1"
+          width="1"
+          alt=""
+          src="//www.googleadservices.com/pagead/conversion/877644049/?label=ZZJHCMaA2nsQkZK_ogM&amp;guid=ON&amp;script=0"
+        />
       </div>
     </noscript>
   </Wrapper>
