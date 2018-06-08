704a63f355dbf398d69b4303e77cd68498be40c2
Rafael Bernardo <rafamaxber@gmail.com>
2018-06-08 00:34:09 -0300

    fix: googleadservices conversion in /obrigado page

--- src/pages/obrigado.js
+++ src/pages/obrigado.js
@@ -67,7 +67,23 @@ const Obrigado = () => (
         Ir para a pagina inicial
       </Btn>
     </ContainerBtns>
-    <span dangerouslySetInnerHTML={{ __html: scriptConversionId }}></span>
+    <script type="text/javascript">
+      {`
+        /* <![CDATA[ */
+        var google_conversion_id = 877644049;
+        var google_conversion_label = "ZZJHCMaA2nsQkZK_ogM";
+        var google_remarketing_only = false;
+        /* ]]> */
+      `}
+    </script>
+
+    <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js"></script>
+
+    <noscript>
+      <div >
+        <img height="1" width="1" alt="" src="//www.googleadservices.com/pagead/conversion/877644049/?label=ZZJHCMaA2nsQkZK_ogM&amp;guid=ON&amp;script=0"/>
+      </div>
+    </noscript>
   </Wrapper>
 );
 
